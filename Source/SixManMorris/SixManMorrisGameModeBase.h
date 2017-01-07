b8afd14344713a5598d352bfafb1e820bd447b1c
ALang <firewolfgo@gmail.com>
2017-01-07 14:35:21 +0800

    init project

---
+++ Source/SixManMorris/SixManMorrisGameModeBase.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameModeBase.h"
+#include "SixManMorrisGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SIXMANMORRIS_API ASixManMorrisGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
